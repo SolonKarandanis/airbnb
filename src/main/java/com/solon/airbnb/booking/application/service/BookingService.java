8a31d42838aaea8044bed7c3d64ec69883bf0089
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-17 16:04:42 +0300

    Created BookingService

---
+++ src/main/java/com/solon/airbnb/booking/application/service/BookingService.java
@@ -0,0 +1,24 @@
+package com.solon.airbnb.booking.application.service;
+
+import com.solon.airbnb.booking.application.dto.BookedDateDTO;
+import com.solon.airbnb.booking.application.dto.BookedListingDTO;
+import com.solon.airbnb.booking.application.dto.NewBookingDTO;
+import com.solon.airbnb.shared.service.State;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookingService {
+
+    public State<Void, String> create(NewBookingDTO newBookingDTO);
+
+    public List<BookedDateDTO> checkAvailability(UUID publicId);
+
+    public List<BookedListingDTO> getBookedListing();
+
+    public State<UUID, String> cancel(UUID bookingPublicId, UUID listingPublicId, boolean byLandlord);
+
+    public List<BookedListingDTO> getBookedListingForLandlord();
+
+    public List<UUID> getBookingMatchByListingIdsAndBookedDate(List<UUID> listingsId, BookedDateDTO bookedDateDTO);
+}
---
+++ src/main/java/com/solon/airbnb/booking/application/service/BookingServiceBean.java
@@ -0,0 +1,47 @@
+package com.solon.airbnb.booking.application.service;
+
+import com.solon.airbnb.booking.application.dto.BookedDateDTO;
+import com.solon.airbnb.booking.application.dto.BookedListingDTO;
+import com.solon.airbnb.booking.application.dto.NewBookingDTO;
+import com.solon.airbnb.shared.service.State;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+public class BookingServiceBean implements BookingService{
+
+
+    @Override
+    public State<Void, String> create(NewBookingDTO newBookingDTO) {
+        return null;
+    }
+
+    @Override
+    public List<BookedDateDTO> checkAvailability(UUID publicId) {
+        return List.of();
+    }
+
+    @Override
+    public List<BookedListingDTO> getBookedListing() {
+        return List.of();
+    }
+
+    @Override
+    public State<UUID, String> cancel(UUID bookingPublicId, UUID listingPublicId, boolean byLandlord) {
+        return null;
+    }
+
+    @Override
+    public List<BookedListingDTO> getBookedListingForLandlord() {
+        return List.of();
+    }
+
+    @Override
+    public List<UUID> getBookingMatchByListingIdsAndBookedDate(List<UUID> listingsId, BookedDateDTO bookedDateDTO) {
+        return List.of();
+    }
+}
