03b3c6036ed9042dc24f2f102c6788f9ebb5b31a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-18 13:39:23 +0300

    Added repos

---
+++ src/main/java/com/solon/airbnb/listing/repository/ListingPictureRepository.java
@@ -0,0 +1,43 @@
+package com.solon.airbnb.listing.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.solon.airbnb.listing.domain.BookingCategory;
+import com.solon.airbnb.listing.domain.Listing;
+import com.solon.airbnb.listing.domain.ListingPicture;
+
+@Repository
+public interface ListingPictureRepository extends JpaRepository<ListingPicture, Long> {
+	
+	@Query("SELECT listing FROM Listing listing LEFT JOIN FETCH listing.pictures picture" +
+            " WHERE listing.landlordPublicId = :landlordPublicId AND picture.isCover = true")
+    List<Listing> findAllByLandlordPublicIdFetchCoverPicture(UUID landlordPublicId);
+	
+	long deleteByPublicIdAndLandlordPublicId(UUID publicId, UUID landlordPublicId);
+
+    @Query("SELECT listing from Listing listing LEFT JOIN FETCH listing.pictures picture" +
+            " WHERE picture.isCover = true AND listing.bookingCategory = :bookingCategory")
+    Page<Listing> findAllByBookingCategoryWithCoverOnly(Pageable pageable, BookingCategory bookingCategory);
+    
+    @Query("SELECT listing from Listing listing LEFT JOIN FETCH listing.pictures picture" +
+            " WHERE picture.isCover = true")
+    Page<Listing> findAllWithCoverOnly(Pageable pageable);
+
+    Optional<Listing> findByPublicId(UUID publicId);
+
+    List<Listing> findAllByPublicIdIn(List<UUID> allListingPublicIDs);
+    
+    Optional<Listing> findOneByPublicIdAndLandlordPublicId(UUID listingPublicId, UUID landlordPublicId);
+
+    Page<Listing> findAllByLocationAndBathroomsAndBedroomsAndGuestsAndBeds(
+            Pageable pageable, String location, int bathrooms, int bedrooms, int guests, int beds
+    );
+}
