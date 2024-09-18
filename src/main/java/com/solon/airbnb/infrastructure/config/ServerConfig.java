3f704247c32e07ecd160a073adf98ca1290f6cc7
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-18 13:54:48 +0300

    Added ApplicationConfig and ServerConfig

---
+++ src/main/java/com/solon/airbnb/infrastructure/config/ServerConfig.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solon.airbnb.infrastructure.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author solon
+ */
+@Configuration
+public class ServerConfig {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector ->{
+        	connector.setProperty("relaxedQueryChars", "[]|{}^&#x5c;&#x60;&quot;&lt;&gt;");
+        });
+        return factory;
+    }
+}
