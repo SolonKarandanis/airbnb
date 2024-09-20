3f15ec05c926036e0bf0f451ae896b9659eeb07d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-20 14:43:46 +0300

    Refactored code

--- src/main/java/com/solon/airbnb/user/application/service/UserDetailsServiceBean.java
+++ src/main/java/com/solon/airbnb/user/application/service/UserDetailsServiceBean.java
@@ -1,11 +1,15 @@
 package com.solon.airbnb.user.application.service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.solon.airbnb.user.application.dto.ReadUserDTO;
 import com.solon.airbnb.user.application.dto.UserDTO;
 import com.solon.airbnb.user.domain.User;
 import com.solon.airbnb.user.repository.UserRepository;
@@ -15,7 +19,7 @@ import com.solon.airbnb.user.repository.UserRepository;
 
 @Service
 @Transactional(readOnly = true)
-public class UserDetailsServiceBean extends BaseUserAccountServiceBean implements UserDetailsService{
+public class UserDetailsServiceBean extends BaseUserAccountServiceBean implements UserDetailsService,UserService{
 	
 	private final UserRepository userRepository;
 
@@ -34,5 +38,17 @@ public class UserDetailsServiceBean extends BaseUserAccountServiceBean implement
 	private UserDTO createSpringSecurityUser(User user) {
 		return convertToDTO(user,true);
     }
+
+	@Override
+	public Optional<ReadUserDTO> getByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<ReadUserDTO> getByPublicId(UUID publicId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
