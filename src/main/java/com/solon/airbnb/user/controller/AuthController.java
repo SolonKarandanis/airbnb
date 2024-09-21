6ffdf733df42dbd736710a720da5f7aefa138a71
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-21 10:55:37 +0300

    Created AuthController

---
+++ src/main/java/com/solon/airbnb/user/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.solon.airbnb.user.controller;
+
+import java.net.URISyntaxException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.solon.airbnb.shared.controller.GenericController;
+import com.solon.airbnb.user.application.dto.JwtDTO;
+import com.solon.airbnb.user.application.dto.SubmitCredentialsDTO;
+import com.solon.airbnb.user.application.dto.UserDTO;
+import com.solon.airbnb.user.application.service.AuthService;
+import com.solon.airbnb.user.application.service.JwtService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/authenticate")
+public class AuthController extends GenericController{
+	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+	private AuthService authService;
+    private JwtService jwtService;
+
+    public AuthController( AuthService authService,JwtService jwtService) {
+        this.authService = authService;
+        this.jwtService = jwtService;
+    }
+
+    /**
+     * {@code POST  /reports} : Create a new report.
+     *
+     * @param submitCredentialsDTO submit credentials.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and
+     * with body the new JwtDTO, or with status {@code 400 (Bad Request)} if the
+     * case has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping
+    public ResponseEntity<JwtDTO> authenticate(@Valid @RequestBody SubmitCredentialsDTO submitCredentialsDTO) {
+    	log.info("AuthController->authenticate----------->username: {}   password: {}",submitCredentialsDTO.getUsername(),submitCredentialsDTO.getPassword());
+    	UserDTO authenticatedUser = authService.authenticate(submitCredentialsDTO);
+    	JwtDTO jwt = jwtService.generateToken(authenticatedUser);
+        return ResponseEntity.ok().body(jwt);
+    }
+}
