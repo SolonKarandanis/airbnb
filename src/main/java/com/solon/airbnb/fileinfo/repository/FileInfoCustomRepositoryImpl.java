ebf5569785c18dbeea70b96db093cb6b1afa48d6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-25 09:37:16 +0300

    Configured querydsl

--- src/main/java/com/solon/airbnb/fileinfo/repository/FileInfoCustomRepositoryImpl.java
+++ src/main/java/com/solon/airbnb/fileinfo/repository/FileInfoCustomRepositoryImpl.java
@@ -1,16 +1,21 @@
 package com.solon.airbnb.fileinfo.repository;
 
 import com.solon.airbnb.fileinfo.domain.FileInfo;
+import com.solon.airbnb.fileinfo.domain.QFileInfo;
 import com.solon.airbnb.shared.exception.AirbnbException;
+import com.solon.airbnb.shared.repository.AbstractRepository;
+import jakarta.persistence.Query;
 
 import java.math.BigInteger;
 import java.util.List;
 
-public class FileInfoCustomRepositoryImpl implements  FileInfoCustomRepository{
+public class FileInfoCustomRepositoryImpl extends AbstractRepository<FileInfo,Long> implements  FileInfoCustomRepository{
+
+    private final QFileInfo fileInfo = QFileInfo.fileInfo;
 
     @Override
     public BigInteger generateIdFromSequencer(String sequencerName) {
-        return null;
+        return super.generateIdFromSequencer(sequencerName);
     }
 
     @Override
@@ -23,8 +28,35 @@ public class FileInfoCustomRepositoryImpl implements  FileInfoCustomRepository{
 
     }
 
+    /**
+     * @param columnName
+     *            is a Triplet with column Name, table Name and Usage in the respective positions.
+     * @return Returns a list of file IDs from the query created by the triplet info.
+     * @throws AirbnbException
+     */
     @Override
     public List<BigInteger> getFileReferenceIdsForTableColumn(String columnName, String tableName) throws AirbnbException {
-        return List.of();
+        List<BigInteger> retVal = null;
+
+        String query = createFileIdsQuery(columnName, tableName);
+        try {
+            Query nsql = getEntityManager().createNativeQuery(query);
+            retVal = nsql.getResultList();
+        } catch (Exception e) {
+            throw new AirbnbException("error.get.referenced.files.from.db.for.table.column", e);
+        }
+        return retVal;
+    }
+
+    /**
+     * @param columnName
+     * @param tableName
+     * @return Returns a query for the file reference ids in the specified column and table that are not null
+     */
+    private String createFileIdsQuery(String columnName, String tableName) {
+        String retVal;
+        retVal = " SELECT o." + columnName + " as filereference " + " "
+                + " FROM " + tableName + " o " + " WHERE o." + columnName + " IS NOT null ";
+        return retVal;
     }
 }
