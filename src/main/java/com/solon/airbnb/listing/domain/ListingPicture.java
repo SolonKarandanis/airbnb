fe7f074c7a36597a1c7978ef383105995df1cd48
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-30 07:35:43 +0300

    Lazy loading Lob

--- src/main/java/com/solon/airbnb/listing/domain/ListingPicture.java
+++ src/main/java/com/solon/airbnb/listing/domain/ListingPicture.java
@@ -5,16 +5,7 @@ import java.util.Objects;
 
 import com.solon.airbnb.shared.domain.AbstractAuditingEntity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "listing_picture")
@@ -32,6 +23,7 @@ public class ListingPicture extends AbstractAuditingEntity<Long> {
 
     @Lob
     @Column(name = "file", nullable = false)
+    @Basic(fetch = FetchType.LAZY)
     private byte[] file;
 
     @Column(name = "file_content_type")
