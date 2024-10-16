4c200fea2e7c6c9ddeb9dedf1d5ef5f233351856
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-16 15:11:30 +0300

    Implemented PictureService

--- src/main/java/com/solon/airbnb/listing/application/service/PictureServiceBean.java
+++ src/main/java/com/solon/airbnb/listing/application/service/PictureServiceBean.java
@@ -2,17 +2,43 @@ package com.solon.airbnb.listing.application.service;
 
 import com.solon.airbnb.listing.application.dto.sub.PictureDTO;
 import com.solon.airbnb.listing.domain.Listing;
+import com.solon.airbnb.listing.domain.ListingPicture;
+import com.solon.airbnb.listing.mapper.ListingPictureMapper;
+import com.solon.airbnb.listing.repository.ListingPictureRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
 public class PictureServiceBean implements PictureService{
 
+    private static final Logger log = LoggerFactory.getLogger(PictureServiceBean.class);
+
+    private final ListingPictureRepository listingPictureRepository;
+    private final ListingPictureMapper listingPictureMapper;
+
+    public PictureServiceBean(ListingPictureRepository listingPictureRepository, ListingPictureMapper listingPictureMapper) {
+        this.listingPictureRepository = listingPictureRepository;
+        this.listingPictureMapper = listingPictureMapper;
+    }
+
+    @Transactional
     @Override
     public List<PictureDTO> saveAll(List<PictureDTO> pictures, Listing listing) {
-        return List.of();
+        Set<ListingPicture> listingPictures = listingPictureMapper.pictureDTOsToListingPictures(pictures);
+        boolean isFirst = true;
+
+        for (ListingPicture listingPicture : listingPictures) {
+            listingPicture.setCover(isFirst);
+            listingPicture.setListing(listing);
+            isFirst = false;
+        }
+        listingPictureRepository.saveAll(listingPictures);
+        return listingPictureMapper.listingPictureToPictureDTO(listingPictures.stream().toList());
     }
 }
