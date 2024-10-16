89c558e75e2ed5b5d50d771d23424cda5383489f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-16 16:28:26 +0300

    Refactored BookingMapper

--- src/main/java/com/solon/airbnb/booking/application/dto/NewBookingDTO.java
+++ src/main/java/com/solon/airbnb/booking/application/dto/NewBookingDTO.java
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public record NewBookingDTO(
-        @NotNull OffsetDateTime startDate,
-        @NotNull OffsetDateTime endDate,
-        @NotNull UUID listingPublicId) {
+        @NotNull String startDate,
+        @NotNull String endDate,
+        @NotNull String listingPublicId) {
 }
--- src/main/java/com/solon/airbnb/booking/mapper/BookingMapper.java
+++ src/main/java/com/solon/airbnb/booking/mapper/BookingMapper.java
@@ -3,26 +3,43 @@ package com.solon.airbnb.booking.mapper;
 import com.solon.airbnb.booking.application.dto.BookedDateDTO;
 import com.solon.airbnb.booking.application.dto.NewBookingDTO;
 import com.solon.airbnb.booking.domain.Booking;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
 
-    Booking newBookingToBooking(NewBookingDTO newBookingDTO);
-
-    @Mapping(target = "startDate", source = "booking", qualifiedByName = "startDate")
-    @Mapping(target = "endDate", source = "booking", qualifiedByName = "endDate")
-    BookedDateDTO bookingToCheckAvailability(Booking booking);
+    @Mapping(target = "startDate", source = "newBookingDTO", qualifiedByName = "startDate")
+    @Mapping(target = "endDate", source = "newBookingDTO", qualifiedByName = "endDate")
+    Booking newBookingToBooking(NewBookingDTO newBookingDTO,@Context DateTimeFormatter dateTimeFormatter);
 
     @Named("startDate")
-    default String mapStartDate(Booking booking){
-        return booking.getStartDate().toString();
+    default OffsetDateTime mapStartDate(NewBookingDTO newBookingDTO,@Context DateTimeFormatter dateTimeFormatter){
+        return OffsetDateTime.parse(newBookingDTO.startDate(), dateTimeFormatter);
     }
 
     @Named("endDate")
-    default String mapEndDate(Booking booking){
+    default OffsetDateTime mapEndDate(NewBookingDTO newBookingDTO,@Context DateTimeFormatter dateTimeFormatter){
+        return OffsetDateTime.parse(newBookingDTO.endDate(), dateTimeFormatter);
+    }
+
+    @Mapping(target = "startDate", source = "booking", qualifiedByName = "startStringDate")
+    @Mapping(target = "endDate", source = "booking", qualifiedByName = "endStringDate")
+    BookedDateDTO bookingToCheckAvailability(Booking booking);
+
+    @Named("startStringDate")
+    default String mapStringStartDate(Booking booking){
+        return booking.getStartDate().toString();
+    }
+
+    @Named("endStringDate")
+    default String mapStringEndDate(Booking booking){
         return booking.getEndDate().toString();
     }
 }
