17a94894fbf4efff3eea7e91ed034b10df89431c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-19 14:19:14 +0200

    TEst data

--- src/test/java/com/solon/airbnb/util/TestUtil.java
+++ src/test/java/com/solon/airbnb/util/TestUtil.java
@@ -1,6 +1,8 @@
 package com.solon.airbnb.util;
 
+import com.solon.airbnb.fileinfo.domain.FileInfo;
 import com.solon.airbnb.shared.common.AuthorityConstants;
+import com.solon.airbnb.shared.dto.Paging;
 import com.solon.airbnb.user.application.dto.UserDTO;
 import com.solon.airbnb.user.domain.Authority;
 import com.solon.airbnb.user.domain.User;
@@ -52,5 +54,25 @@ public class TestUtil {
 
     public static Authentication getTestAuthenticationFromUserDTO(UserDTO userDto) {
         return getTestAuthenticationFromUserDTO(userDto, TestConstants.TEST_TOKEN);
+    }
+
+    public static Paging createPaging(int pagingSize, int pagingIndex, String sortCol, String sortDirection) {
+        Paging output = new Paging();
+        output.setPagingSize(pagingSize);
+        output.setPagingStart(pagingIndex);
+        output.setSortingColumn(sortCol);
+        output.setSortingDirection(sortDirection);
+        return output;
+    }
+
+    public static FileInfo generateTestFileInfo() {
+        FileInfo file = new FileInfo();
+        file.setId(2L);
+        file.setMimeType("application/octet-stream");
+        file.setFileName("CATALOGUE_HISTORY_FILE");
+        file.setFileRefId(2L);
+        file.setFileName("catalogue_v.0.1.xls");
+        file.setFileSize(15872);
+        return file;
     }
 }
