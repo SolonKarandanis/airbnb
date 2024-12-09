b4210286544444f137286f68f360593589e44c51
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-09 10:36:35 +0200

    Implementing TenantControllerTest

--- src/test/java/com/solon/airbnb/listing/controller/TenantControllerTest.java
+++ src/test/java/com/solon/airbnb/listing/controller/TenantControllerTest.java
@@ -1,4 +1,30 @@
 package com.solon.airbnb.listing.controller;
 
+
+import com.solon.airbnb.listing.application.dto.DisplayCardListingDTO;
+import com.solon.airbnb.listing.application.service.TenantService;
+import com.solon.airbnb.util.TestUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("TenantControllerTest")
+@ExtendWith(MockitoExtension.class)
 public class TenantControllerTest {
+
+    @InjectMocks
+    protected TenantController controller;
+
+    @Mock
+    protected TenantService tenantService;
+
+    protected DisplayCardListingDTO dto;
+
+    @BeforeEach
+    public void setup(){
+        dto = TestUtil.generateDisplayCardListingDTO();
+    }
 }
